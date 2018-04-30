3d984705feaef567486dcd4ae98dd3d16b904d61
Bogdan Orzea <bogdan.orzea@gmail.com>
2018-04-30 19:57:44 +0300

    Experiment with Retrofit

--- app/src/main/java/com/bogdanorzea/bakingapp/data/ReceiptsRepository.java
+++ app/src/main/java/com/bogdanorzea/bakingapp/data/ReceiptsRepository.java
@@ -1,4 +1,49 @@
 package com.bogdanorzea.bakingapp.data;
 
+import com.bogdanorzea.bakingapp.data.database.Receipt;
+import com.bogdanorzea.bakingapp.data.network.ResponseApiService;
+
+import java.util.List;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import timber.log.Timber;
+
 public class ReceiptsRepository {
+
+    public void getReceipts() {
+        String API_BASE_URL = "https://d17h27t6h515a5.cloudfront.net";
+
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(API_BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient.build())
+                .build();
+
+        ResponseApiService apiService = retrofit.create(ResponseApiService.class);
+
+        Call<List<Receipt>> call = apiService.getReceipts();
+        call.enqueue(new Callback<List<Receipt>>() {
+            @Override
+            public void onResponse(Call<List<Receipt>> call, Response<List<Receipt>> response) {
+                if (response.isSuccessful()) {
+                    for (Receipt receipt: response.body()) {
+                        Timber.d(receipt.getName());
+                    }
+                } else {
+                    Timber.e("Response code %s and raw body: %s.", response.code(), response.raw());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Receipt>> call, Throwable t) {
+                Timber.d(t.toString());
+            }
+        });
+    }
 }
--- app/src/main/java/com/bogdanorzea/bakingapp/data/network/ResponseApiService.java
+++ app/src/main/java/com/bogdanorzea/bakingapp/data/network/ResponseApiService.java
@@ -1,6 +1,6 @@
 package com.bogdanorzea.bakingapp.data.network;
 
-import com.bogdanorzea.bakingapp.data.database.ReceiptEntity;
+import com.bogdanorzea.bakingapp.data.database.Receipt;
 
 import java.util.List;
 
@@ -8,6 +8,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface ResponseApiService {
-    @GET("https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json")
-    Call<List<ReceiptEntity>> getReceipts();
+    @GET("/topher/2017/May/59121517_baking/baking.json1")
+    Call<List<Receipt>> getReceipts();
 }
