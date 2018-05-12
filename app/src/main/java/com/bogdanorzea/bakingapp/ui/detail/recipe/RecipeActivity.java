74d4024166b6439be1343523f24af7aabd86480d
Bogdan Orzea <bogdan.orzea@gmail.com>
2018-05-12 18:08:14 +0300

    Move logic from the RecipeActivity to the RecipeFragment

--- app/src/main/java/com/bogdanorzea/bakingapp/ui/detail/recipe/RecipeActivity.java
+++ app/src/main/java/com/bogdanorzea/bakingapp/ui/detail/recipe/RecipeActivity.java
@@ -1,27 +1,20 @@
 package com.bogdanorzea.bakingapp.ui.detail.recipe;
 
-import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-import com.bogdanorzea.bakingapp.InjectorUtils;
 import com.bogdanorzea.bakingapp.R;
-import com.bogdanorzea.bakingapp.ui.detail.step.StepActivity;
 
-import timber.log.Timber;
-
-public class RecipeActivity extends AppCompatActivity implements StepAdapter.OnItemClickHandler {
+public class RecipeActivity extends AppCompatActivity {
 
     public static final String RECIPE_ID = "RECIPE_ID";
     private int mId = -1;
-    private RecipeActivityViewModel mViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,39 +34,23 @@ public class RecipeActivity extends AppCompatActivity implements StepAdapter.OnI
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Intent intent = getIntent();
-        if (intent != null) {
-            if (intent.hasExtra(RECIPE_ID)) {
-                mId = intent.getIntExtra(RECIPE_ID, -1);
-            }
+        if (intent != null && intent.hasExtra(RECIPE_ID)) {
+            mId = intent.getIntExtra(RECIPE_ID, -1);
+        } else {
+            finish();
         }
 
-        RecyclerView recyclerView = findViewById(R.id.recipe_content_list);
-        recyclerView.setLayoutManager(
-                new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-        //IngredientAdapter adapter = new IngredientAdapter(this);
-        StepAdapter adapter = new StepAdapter(this, this);
-        recyclerView.setAdapter(adapter);
+        if (savedInstanceState == null) {
+            RecipeActivityFragment recipeActivityFragment = new RecipeActivityFragment();
 
-        if (mId != -1) {
-            RecipeActivityViewModelFactory factory = InjectorUtils.provideDetailViewModelFactory(this, mId);
-            mViewModel = ViewModelProviders.of(this, factory).get(RecipeActivityViewModel.class);
-//            mViewModel.getIngredients().observe(this, ingredients -> {
-//                adapter.swapIngredients(ingredients);
-//            });
-            mViewModel.getRecipe().observe(this, newRecipe -> {
-                adapter.swapSteps(newRecipe.steps);
-            });
+            Bundle bundle = new Bundle();
+            bundle.putInt(RecipeActivityFragment.RECIPE_ID, mId);
+            recipeActivityFragment.setArguments(bundle);
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_recipe_details, recipeActivityFragment, "FRAGMENT_RECIPE")
+                    .commit();
         }
-    }
-
-    @Override
-    public void onItemClick(int stepId) {
-        Timber.d("Step at position %s was clicked", stepId);
-
-        Intent intent = new Intent(this, StepActivity.class);
-        intent.putExtra(StepActivity.RECIPE_ID, mId);
-        intent.putExtra(StepActivity.STEP_ID, stepId);
-
-        startActivity(intent);
     }
 }
--- app/src/main/java/com/bogdanorzea/bakingapp/ui/detail/recipe/RecipeActivityFragment.java
+++ app/src/main/java/com/bogdanorzea/bakingapp/ui/detail/recipe/RecipeActivityFragment.java
@@ -1,17 +1,32 @@
 package com.bogdanorzea.bakingapp.ui.detail.recipe;
 
-import android.support.v4.app.Fragment;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.bogdanorzea.bakingapp.InjectorUtils;
 import com.bogdanorzea.bakingapp.R;
+import com.bogdanorzea.bakingapp.ui.detail.step.StepActivity;
+
+import timber.log.Timber;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class RecipeActivityFragment extends Fragment {
+public class RecipeActivityFragment extends Fragment
+        implements StepAdapter.OnItemClickHandler {
+
+    public static final String RECIPE_ID = "RECIPE_ID";
+    private RecipeActivityViewModel viewModel;
+    private int recipeId;
 
     public RecipeActivityFragment() {
     }
@@ -19,6 +34,44 @@ public class RecipeActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+        Context context = getContext();
+
+
+        RecyclerView recyclerView = view.findViewById(R.id.recipe_content_list);
+        recyclerView.setLayoutManager(
+                new LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false));
+        StepAdapter adapter = new StepAdapter(context, this);
+        recyclerView.setAdapter(adapter);
+
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            recipeId = bundle.getInt(RECIPE_ID, -1);
+
+            Toast.makeText(getContext(), "" + recipeId, Toast.LENGTH_SHORT).show();
+        }
+
+        if (recipeId != -1) {
+            RecipeActivityViewModelFactory factory = InjectorUtils.provideDetailViewModelFactory(context, recipeId);
+            viewModel = ViewModelProviders.of(this, factory).get(RecipeActivityViewModel.class);
+            viewModel.getRecipe().observe(this, newRecipe -> {
+                adapter.swapSteps(newRecipe.steps);
+            });
+        }
+
+        return view;
+    }
+
+    @Override
+    public void onItemClick(int stepId) {
+        Timber.d("Step at position %s was clicked", stepId);
+        Context context = getContext();
+
+        Intent intent = new Intent(context, StepActivity.class);
+        intent.putExtra(StepActivity.RECIPE_ID, recipeId);
+        intent.putExtra(StepActivity.STEP_ID, stepId);
+
+        startActivity(intent);
     }
 }
