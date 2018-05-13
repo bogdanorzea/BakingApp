f88cb95f42eaace022f81c77ad673de240500ee0
Bogdan Orzea <bogdan.orzea@gmail.com>
2018-05-13 22:20:41 +0300

    Adds the ability to create widgets

---
+++ app/src/main/java/com/bogdanorzea/bakingapp/IngredientsWidgetProvider.java
@@ -0,0 +1,52 @@
+package com.bogdanorzea.bakingapp;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.bogdanorzea.bakingapp.ui.detail.StepListActivity;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class IngredientsWidgetProvider extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.ingredients_widget);
+
+        Intent intent = new Intent(context, StepListActivity.class);
+        intent.putExtra(StepListActivity.RECIPE_ID, 1); // TODO Remove hardcoded RECIPE_ID
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        views.setOnClickPendingIntent(R.id.image_view, pendingIntent);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
